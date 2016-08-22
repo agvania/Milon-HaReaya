3100df29c72a22085670cc19d0de02007cc5cd14
agvania <shlomidaudi@gmail.com>
2016-08-22 10:40:45 +0300

    Basic functionality

--- check_footnotes.py
+++ check_footnotes.py
@@ -62,27 +62,64 @@ def findParagraph(tree, child):
 	return None
 
 def text(elem):
-	return reduce(lambda a,b: a+b, next_textual.itertext())
+	if isinstance(elem, ET.Element):
+		return reduce(lambda a,b: a+b, elem.itertext(), '')
+	elif isinstance(elem, type([])):
+		return reduce(lambda e1,e2: text(e1)+text(e2), elem, '')
+	else:
+		return elem
+
+def docxtext(docx_elements):
+	return reduce(lambda e1,e2: e1.text+e2.text, docx_elements)
 
 reffed_fns = {}
 
 with zipfile.ZipFile(milon, 'r') as docxzip:
 	tree = ET.ElementTree(ET.fromstring(docxzip.open('word/document.xml', 'r').read()))
 	for n in note:
+		# find the xml element representing the note
 		ref = tree.find(".//%s[@%s='%d'].." % (dtag.footnoteReference, dtag.id, n.id))
 		if ref is not None:
+			# find the paragraph containing the footnote
 			p = tree.find(".//%s[@%s='%d']../.." % (dtag.footnoteReference, dtag.id, n.id))
+			# find the next element right after the footnote
 			next = findNext(p, ref)
+			# if it's a 'bookmarkEnd' element, it means this footnote is referenced somewhere along the rest of the document
 			if next.tag == dtag.bookmarkEnd:
+				# get the bookmark's unique name from its 'bookmarkStart' element
 				ref_name = p.find(".//%s[@%s='%s']" % (dtag.bookmarkStart, dtag.id, next.attrib[dtag.id])).attrib[dtag.name][1:]
+				# find the elements referencing this footnote
 				ref_elems = tree.findall(".//*/[%s='%s']" % (dtag.instrText , ref_name + ' \h'))
 				if ref_elems: # not empty
-					#print "%d: %s" % (n.id, ref_name)
 					text_list = []
+					# for each element, find the next textual element after it, and get its text,
+					# which is (or should be at least), the footnote number of the referring footnote
 					for e in ref_elems:
 						pp = findParagraph(tree, e)
 						next_textual = findNextTextual(pp, e)
-						text_list.append(text(next_textual))
+						recurring_fn_n = text(next_textual).strip()
+						p_r = list(pp.iter(dtag.r))
+						text_list.append(text(p_r))
 					reffed_fns[n.id] = (ref_name, text_list)
 
-pprint(reffed_fns)
+for k,v in reffed_fns.iteritems():
+	print k, v[0]
+	for u in v[1]:
+		print u
+
+
+
+
+'''
+TODO:
+There's a problem here yet: all I do is to find the paragraphs referring to each footnote.
+But what we really want is the places where the referring footnote number is not the same as the original footnote.
+The original footnote's number is not written in the xml. It is determined by word when processing the xml.
+That's why the number is changing automatically when new footnotes are added.
+But the number of the referring footnotes ARE written in the xml.
+So to really know if the referring footnote's number differs from the original,
+we need to calculate the number of the original with the same logic of word.
+I don't know how word knows when to start a new numbering. That's what we need to find out.
+My guess is that it has a special element in the xml to tell that a new section has started,
+and thus the following footnotes will start with a new number.
+'''
